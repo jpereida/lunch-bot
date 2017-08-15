be9e4ab74bdb9629f3aedf7b154a23f0bf17e14d
Jerry Pereida <jerrypereida@gmail.com>
2017-08-15 00:12:28 -0500

    Create one sql file for scripts

---
+++ code/db-scripts.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS LunchBot;
+CREATE DATABASE LunchBot;
+
+CREATE TABLE lunch_menu(
+  lunch_date DATE NOT NULL,
+  primary_meal varchar(700) NOT NULL,
+  secondary_meal varchar(700) DEFAULT NULL
+);
+
+INSERT INTO lunch_menu(lunch_date, primary_meal, secondary_meal)
+VALUES
+("2017-08-15", "Spaghetti", NULL);
--- code/lunch_menu.sql
+++
@@ -1,5 +0,0 @@
-create table lunch_menu(
-  lunch_date DATE NOT NULL,
-  primary_meal varchar(700) NOT NULL,
-  secondary_meal varchar(700) DEFAULT NULL
-);
